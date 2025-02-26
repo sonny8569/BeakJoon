978bb3814be1d395acba7de895761004be44151b
KIM SUNGIL <82859578+sonny8569@users.noreply.github.com>
2025-02-26 15:45:38 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int data = Integer.parseInt(br.readLine());
+        System.out.println(data - 543);
+    }
+
+
+}
