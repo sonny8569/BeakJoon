1ed1698739bc8038b9ac0268be2c46234f536c7a
KIM SUNGIL <82859578+sonny8569@users.noreply.github.com>
2025-07-07 11:56:08 +0900

    [Bronze V] Title: A+B, Time: 92 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.kt
@@ -0,0 +1,16 @@
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class CodingTest {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val br = BufferedReader(InputStreamReader(System.`in`))
+            val input = br.readLine().split(" ")
+            val first = input[0]
+            val sec = input[1]
+            print(first.toInt() + sec.toInt())
+        }
+    }
+}
