591be44f26ccc087a7606f2bafb37d30f6762095
KIM SUNGIL <82859578+sonny8569@users.noreply.github.com>
2025-07-07 17:18:53 +0900

    [Bronze V] Title: Hello World, Time: 72 ms, Memory: 11936 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.kt
@@ -0,0 +1,11 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class CodingTest {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello World!")
+        }
+    }
+}
