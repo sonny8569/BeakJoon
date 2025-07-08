588df4d36d4c9e751a4ddf9d8f064c7e905af222
KIM SUNGIL <82859578+sonny8569@users.noreply.github.com>
2025-07-08 17:00:08 +0900

    [Bronze V] Title: 고양이, Time: 76 ms, Memory: 12116 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.kt
@@ -0,0 +1,11 @@
+class CodingTest {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("\\    /\\\n" +
+                    " )  ( ')\n" +
+                    "(  /  )\n" +
+                    " \\(__)|")
+        }
+    }
+}
