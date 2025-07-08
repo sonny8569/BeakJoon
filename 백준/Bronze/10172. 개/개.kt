562a88e258d4d59fc6e3cadf6c720ae41709e17f
KIM SUNGIL <82859578+sonny8569@users.noreply.github.com>
2025-07-08 17:00:54 +0900

    [Bronze V] Title: 개, Time: 68 ms, Memory: 11936 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.kt
@@ -0,0 +1,12 @@
+class CodingTest {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("|\\_/|\n" +
+                    "|q p|   /}\n" +
+                    "( 0 )\"\"\"\\\n" +
+                    "|\"^\"`    |\n" +
+                    "||_/=\\\\__|")
+        }
+    }
+}
