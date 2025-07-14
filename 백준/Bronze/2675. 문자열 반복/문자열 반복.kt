96962a397319a7c1dc009f0a0f75493d6129c987
KIM SUNGIL <82859578+sonny8569@users.noreply.github.com>
2025-07-14 22:26:05 +0900

    [Bronze II] Title: 문자열 반복, Time: 108 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.kt
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.kt
@@ -1,27 +1,25 @@
-
 class CodingTest {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
             val reader = System.`in`.bufferedReader()
             val length = reader.readLine().toInt()
-            val answer = ArrayList<String>()
-            var answerStr = ""
-            for (i in 0..length - 1) {
-                val input = reader.readLine().split(" ")
-                val repeat = input[0].toInt()
-                val data = input[1]
-                for (j in data) {
-                    for (k in 0..repeat - 1) {
-                        answerStr += j
+            val answer = mutableListOf<String>()
+
+            for (i in 0 until length) {
+                val (repeatStr, word) = reader.readLine().split(" ")
+                val repeat = repeatStr.toInt()
+                val builder = StringBuilder()
+
+                for (char in word) {
+                    repeat(repeat) {
+                        builder.append(char)
                     }
                 }
-                answer.add(answerStr)
-                answerStr = ""
+                answer.add(builder.toString())
             }
-            for (data in answer) {
-                println(data)
-            }
+
+            answer.forEach { println(it) }
         }
     }
 }
