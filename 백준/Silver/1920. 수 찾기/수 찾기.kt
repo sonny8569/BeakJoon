1228545107bd9e73bd1d8bb21e615eb3b845f7c3
KIM SUNGIL <82859578+sonny8569@users.noreply.github.com>
2025-07-28 22:36:30 +0900

    [Silver IV] Title: 수 찾기, Time: 1948 ms, Memory: 110416 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.kt
@@ -0,0 +1,19 @@
+class CodingTest {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val reader = System.`in`.bufferedReader()
+            val targetIndex = reader.readLine().toInt()
+            val targetData = reader.readLine().split(" ").map { data -> data.toInt() }.associateWith { true }
+            val findIndex = reader.readLine().toInt()
+            val findData = reader.readLine().split(" ").map { data -> data.toInt() }
+            for (data in findData) {
+                if(data in targetData.keys && targetData[data] == true) {
+                    println("1")
+                } else {
+                    println("0")
+                }
+            }
+        }
+    }
+}
